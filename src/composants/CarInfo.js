4d479ecac0d88592939ad54cf1e07fc0305c48b9
abiola <abiole68@gmail.com>
2024-04-11 22:38:01 +0000

    modification 22:37 11 04 2024

--- src/composants/CarInfo.js
+++ src/composants/CarInfo.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
-import FontAwesome6Icon from 'react-native-vector-icons/FontAwesome6';
+import { View, StyleSheet, TouchableOpacity, Image, Text, ScrollView } from 'react-native';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const CarInfo = ({route, navigation}) => {
-    const {info} = route.params
-    console.log(info)
+const CarInfo = ({ route, navigation }) => {
+    const { info } = route.params
     return (
-        <View>
+        <ScrollView>
 
-            <TouchableOpacity>
-                <FontAwesome6Icon name="arrow-left" style={{fontsize:22, fontWeight:"bold", margin:20}}/>
+            <TouchableOpacity style={{zIndex:999}}>
+                <FontAwesome6 name="arrow-left" style={{ fontSize: 22, fontWeight: "bold", margin: 20 }} onPress={() => navigation.goBack()} />
             </TouchableOpacity>
-            
-        </View>
+
+            <View>
+                <Image source={info.img} style={{ width: "100%", height: 400,marginTop:-60 }} />
+
+                <View style={{ flexDirection: 'row', alignItems: "center" }}>
+                        <FontAwesome name='star' style={{ fontSize: 18, color: "orange" }} />
+                        <FontAwesome name='star' style={{ fontSize: 18, color: "orange" }} />
+                        <FontAwesome name={info.star >= 3 ? "star" : "star-o"} style={{ fontSize: 18, color: "orange" }} />
+                        <FontAwesome name={info.star >= 4 ? "star" : "star-o"} style={{ fontSize: 18, color: "orange" }} />
+                        <FontAwesome name={info.star >= 5 ? "star" : "star-o"} style={{ fontSize: 18, color: "orange" }} />
+                    </View>
+
+                <Text style={{ fontSize: 22, fontWeight: 700, textAlign: "center", marginTop: 20, marginBottom: 20 }}> {info.title}</Text>
+                <Text style={{ fontSize: 18, fontWeight: 300, marginBottom: 20 }}> {info.description} </Text>
+                <Text style={{ textAlign: "justify", marginHorizontal: 5 }}> {info.info} </Text>
+            </View>
+
+        </ScrollView>
     );
 }
 
